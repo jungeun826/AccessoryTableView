0339a06bd6686717dc6a327c0a865602d573f34c
jungeun826 <jungeun826@nate.com>
2014-01-06 17:19:51 +0900

    174페이지 실습 완료했다.

--- AccessoryTableView/ViewController.m
+++ AccessoryTableView/ViewController.m
@@ -7,17 +7,46 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#define CELL_ID @"CELL_ID"
+@interface ViewController ()<UITableViewDataSource, UITabBarDelegate>
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSArray *data;
+}
+//셀 선택시 log확인
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"Select Row : %d", (int)indexPath.row);
+}
+//악세서리 버튼 클릭시
+- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"Accessory Button tapped : %d", (int)indexPath.row);
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID forIndexPath:indexPath];
+    
+    if(indexPath.row == 0)
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    else if(indexPath.row == 1)
+        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    else if(indexPath.row == 2)
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    else if(indexPath.row == 3)
+        cell.accessoryType = UITableViewCellAccessoryDetailButton;
+    cell.textLabel.text = data[indexPath.row];
+    
+    return cell;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data = @[@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i"];
 }
 
 - (void)didReceiveMemoryWarning
